0824b966409161d998682a391f7622ddf1d6aa6a
Suvasis Pyne <spyne@thar.(none)>
2012-08-09 18:49:20 +0530

    Sign-in-out without cucumber

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 SampleApp::Application.routes.draw do
-  
+
   get "static_pages/home"
-  
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-  resources :users
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
