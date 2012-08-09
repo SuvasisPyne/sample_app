02e8baa1f1b162403a554b7d4af34c825053f3b2
Suvasis Pyne <spyne@thar.(none)>
2012-08-09 11:57:24 +0530

    Finish User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
   end
+  def create
+   @user = User.new(params[:user])
+   if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+   else
+      render 'new'
+   end
+  end
 end
