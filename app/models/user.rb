0ee8a4990627dce9bd9872d1a08cd964481d8c2d
SuvasisPyne <suvopyne49@gmail.com>
2012-08-16 18:39:24 +0530

    Followed Users and Followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships#, source: :follower
   has_secure_password
 
   before_save { |u| u.email = email.downcase }
@@ -28,14 +32,24 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
 
   private
 
    def create_remember_token
      self.remember_token = SecureRandom.urlsafe_base64
    end
-
-
 
 
 end
