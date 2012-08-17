7e64c4cfd2d89bf2b31d035efd31708f4c69b823
SuvasisPyne <suvopyne49@gmail.com>
2012-08-17 18:37:50 +0530

    Completed followers/following and news feed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/home"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 
   match '/signup',  to: 'users#new'
